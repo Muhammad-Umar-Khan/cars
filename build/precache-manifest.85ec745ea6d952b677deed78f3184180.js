905c33911491e86ba300a40db34cbae8de10c558
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-11-18 09:34:52 +0500

    all files

---
+++ build/precache-manifest.85ec745ea6d952b677deed78f3184180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b2d191d02f1331830064eb1888ce7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d579ddccc9dfd81fa67",
+    "url": "/static/css/2.9323a969.chunk.css"
+  },
+  {
+    "revision": "4d579ddccc9dfd81fa67",
+    "url": "/static/js/2.fec55b4d.chunk.js"
+  },
+  {
+    "revision": "6a6205428fa9da93db7d40597918f75d",
+    "url": "/static/js/2.fec55b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7609040da87b439cd0b",
+    "url": "/static/js/main.481ce726.chunk.js"
+  },
+  {
+    "revision": "85ea0fb72a380a975252",
+    "url": "/static/js/runtime-main.98d5a068.js"
+  }
+]);
